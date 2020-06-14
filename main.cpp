7b11db397fefe2ba8ed36e1a04c3c0e51965a054
dboyliao <qmalliao@gmail.com>
2020-06-14 23:32:49 +0800

    fix bug. it's initializer list, babe

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
-#include "models/my_model/my_model.hpp"  //gernerated model file"
-#include "uTensor/src/uTensor.h"
+#include <stdio.h>
+
 #include <cmath>
 #include <iostream>
-#include "mbed.h"
-#include <stdio.h>
+
 #include "input_image.h"  //contains the first sample taken from the MNIST test set
+#include "mbed.h"
+#include "models/my_model/my_model.hpp"  //gernerated model file"
+#include "uTensor.h"
 
 using namespace uTensor;
 
-void onError(Error* err) {
+void onError(Error *err) {
   while (true) {
   }
 }
 
-int argmax(const Tensor& logits) {
+int argmax(const Tensor &logits) {
   uint32_t num_elems = logits->num_elems();
   float max_value = static_cast<float>(logits(0));
   int max_index = 0;
@@ -27,12 +29,11 @@ int argmax(const Tensor& logits) {
   return max_index;
 }
 
-
 // FIXME: estimated_meta_usage and estimated_ram_usage need to be gernerated
 const int estimated_ram_usage = 5000;
 const int estimated_meta_usage = 1000;
 
-Serial pc(USBTX, USBRX, 115200);  //baudrate := 115200
+Serial pc(USBTX, USBRX, 115200);  // baudrate := 115200
 localCircularArenaAllocator<estimated_meta_usage> meta_allocator;
 localCircularArenaAllocator<estimated_ram_usage, uint32_t> ram_allocator;
 SimpleErrorHandler mErrHandler(10);
@@ -45,19 +46,19 @@ int main(void) {
   Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
   Context::get_default_context()->set_ErrorHandler(&mErrHandler);
   static My_model model;
-  
+
   // create the input/output tensor
   Tensor input_image = new RomTensor({1, 28, 28, 1}, flt, arr_input_image);
   Tensor logits = new RamTensor({1, 10}, flt);
 
-  model.set_inputs({My_model::input_0, input_image})
-        .set_outputs({My_model::output_0, logits})
-        .eval();
+  model.set_inputs({{My_model::input_0, input_image}})
+      .set_outputs({{My_model::output_0, logits}})
+      .eval();
 
   int max_index = argmax(logits);
   input_image.free();
   logits.free();
-  
+
   printf("pred label: %d\r\n", max_index);
 
   return 0;
